943f7e69803bb51da4880c9c108f16319aaccc87
Darshan-Kantesariya-55 <ifour.darshank@gmail.com>
2025-02-04 18:10:44 +0530

    ai code service 2

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,7 +4,8 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { SpeechRecognitionService } from './services/speech-recognition.service';
+import { OpenaiService } from './services/openai.service';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration(), SpeechRecognitionService]
+  providers: [provideRouter(routes), provideClientHydration(), SpeechRecognitionService, OpenaiService]
 };
